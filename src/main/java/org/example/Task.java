b9160d6a7dc11cf37b24eceda814582f26062c51
oleksandr-matvieiev <oleksandr.matvieiev.d@gmail.com>
2025-09-01 12:02:27 +0200

    feat: replace String status with TaskStatus enum and update Task model

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -1,31 +1,77 @@
 package org.example;
 
+
+import jakarta.validation.constraints.*;
+
 import java.time.LocalDate;
 
 public class Task {
-    private final int id;
-    private final String title;
-    private final String description;
-    private final LocalDate endDate;
-    private final String status;
+    private int id;
+    @NotNull
+    @Size(min = 1, max = 50)
+    private  String title;
+    @NotNull
+    @Size(min = 1, max = 50)
+    private  String description;
+    @NotNull
+    @Future
+    private LocalDate endDate;
+    @NotNull
+    private  TaskStatus status;
 
 
+    public Task() {}
 
-    //Constructor for load from DB
-    public Task(int id, String title, String description, LocalDate endDate, String status) {
-        this.id = id;
+    public Task(String title, String description, LocalDate endDate, TaskStatus status) {
         this.title = title;
         this.description = description;
         this.endDate = endDate;
         this.status = status;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
 
     @Override
     public String toString() {
         return "Id: " + id + "\nTitle: " + title + "\nDescription: "
-                + description + "\nEndDate: " + endDate + "\nStatus: " + status;
+                + description + "\nEndDate: " + endDate + "\nStatus: " + status.toString();
     }
 }
